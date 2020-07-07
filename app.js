797506eca82381852dddee304fb7e567609ffebb
warrioryoko <miangpellegri@gmail.com>
2020-07-07 14:50:04 -0700

    Got some of the main function and logic complete.

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 // import functions and grab DOM elements
+import { compareNumbers } from './utils.js';
 
+const userInput = document.getElementById('number-goes-here');
+const submitButton = document.getElementById('press-button');
+const guessesLeft = document.getElementById('guesses-left');
+const userGuessedInput = document.getElementById('user-guessed');
+const finalResults = document.getElementById('final-results');
+const resetButton = document.getElementById('reset-button');
 // initialize state
-
+const correctNumber = Math.ceil(Math.random() * 20);
+let guessCount = 4;
 // set event listeners to update state and DOM
+
+function handleGuess() {
+  const userGuessed = Number(userInput.value);
+
+  function guessMatch() {
+    finalResults.textContent = "You guessed right!"
+  }
+
+  function noGuessesLeft() {
+    finalResults.textContent = "Wrong again... and you're out of guesses! You Lose!";
+  }
+
+  function wrongGuess() {
+    guessCount--;
+    if (guessCount <= 0) {
+      noGuessesLeft();
+    }
+  }
+
+  function showResults(wrongNumber) {
+   
+  }
+}
