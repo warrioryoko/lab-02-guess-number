d3bfbdf3f46490966c08598c7af13f3dd7750be9
warrioryoko <miangpellegri@gmail.com>
2020-07-07 13:57:29 -0700

    Basic HTML skeleton, and compare function finished.

---
+++ utils.js
@@ -0,0 +1,14 @@
+export function compareNumbers(guess, correctNumber) {
+  if (guess === correctNumber) {
+    // instructions say to return 0 for an identical match
+    return 0;
+  } else if (guess > correctNumber) {
+    // return 1 for a guess higher than the random number
+    return 1;
+  } else if (guess < correctNumber) {
+    // return -1 for a guess lower than the random number
+    return -1;
+  } else {
+    alert('somehow you managed to get a value that is not a number...');
+  }
+}
